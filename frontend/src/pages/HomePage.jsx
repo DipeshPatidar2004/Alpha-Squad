cd7275d3ad1dc13b23f73c66a920afae6f4ee2c1
Dipesh Patidar <dipupatidar.6264@gmail.com>
2026-01-02 17:59:57 +0530

    Replace HomePage content with EduForAllPage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import EduForAllPage from "EduForAllPage.jsx";
 export default function HomePage() {
-  return <h1 className="text-white text-3xl p-10">This is Home Page</h1>;
+  return <h1 className="text-white text-3xl p-10"><EduForAllPage/></h1>;
 }
