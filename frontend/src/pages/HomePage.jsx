c83b38f32c68a3148614c31d741a4ae5b96ed33d
Dipesh Patidar <dipupatidar.6264@gmail.com>
2026-01-02 18:00:48 +0530

    Simplify HomePage component rendering

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
 import EduForAllPage from "EduForAllPage.jsx";
 export default function HomePage() {
-  return <h1 className="text-white text-3xl p-10"><EduForAllPage/></h1>;
+  return <EduForAllPage/>;
 }
